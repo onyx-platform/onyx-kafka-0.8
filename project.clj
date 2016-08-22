d1704cf258638eb21dc2ac7babf3bb80489e4844
OnyxBot <michael.drogalis@onyxplatform.org>
2016-08-22 00:37:20 +0000

    Update dependency org.onyxplatform/onyx to version 0.9.10-20160822_002742-g25374be.
    Automatic commit by lein-unison.

--- project.clj
+++ project.clj
@@ -13,7 +13,7 @@
                              :sign-releases false}}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  ^{:voom {:repo "git@github.com:onyx-platform/onyx.git" :branch "master"}}
-                 [org.onyxplatform/onyx "0.9.10-20160802_130201-g32147f4"]
+                 [org.onyxplatform/onyx "0.9.10-20160822_002742-g25374be"]
                  [clj-kafka "0.3.4" :exclusions [org.apache.zookeeper/zookeeper zookeeper-clj]]
                  [com.stuartsierra/component "0.2.3"]
                  [cheshire "5.5.0"]
